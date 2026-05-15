caffe815b48219d925f7a80353114a532628894d
Robson Mariano <rml221082@gmail.com>
2026-05-15 16:18:21 -0300

    Atualização do site

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
     window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbx4T9_Bz_FKnVFQxKSIsTmx2GdeAQj-smxOXUeAsViTGmAR9JTnUGbYlwGCnIdGDxMSkw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyYyVj15g0e5KJO6d01pG6n7849fc3bs8RuwdbCGTfkIn0DAoNJKfbDpxqDu4o8t-bW1g/exec",
   VERSION: "1.0",
   TIMEOUT: 5000
 };
