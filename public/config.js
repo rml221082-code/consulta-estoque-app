66a3d9892c03c2f4edd77890d90805c7d59413cd
Robson Mariano <rml221082@gmail.com>
2026-06-03 16:51:18 -0300

    Atualização do site

--- public/config.js
+++ public/config.js
@@ -8,7 +8,7 @@
 
 
 window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwMVPrDXfHHPws8d5BAEQ0kz3hoxOomstK_mxX-IWYUWdoDoXTqhjxwiIMpsGG1vo9T6g/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyVn2LtZ6UgXl-xDqlbuIFcABk7DxAlmMQjtIRCyHZzRAjzBaQXZsqDRDFAJ6GNAaGFfw/exec",
   VERSION: "1.0",
   TIMEOUT: 5000
 };
